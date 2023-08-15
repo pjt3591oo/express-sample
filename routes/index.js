bc9a0cf1397c657ec3ae79b30139f6251a3a78fa
pjt3591oo <pjt3591oo@gmail.com>
2023-08-15 13:13:05 +0900

    console 추가

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,31 @@ const express = require('express');
 const pool = require('../db');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', async (req, res, next) => {
-  const promisePool = pool.promise();
-  const [ rows, fields ] = await promisePool.query('SELECT * FROM t');
-  return res.json(rows);
-});
-
-router.get('/save', async (req, res) => {
-  const { name } = req.query;
-
-  const promisePool = pool.promise();
-  const [ rows, fields ] = await promisePool.query('INSERT INTO t (name) VALUES(?)', [name]);
-
-  return res.json(rows);
-})
-
 router.get('/env', (req, res) => {
+  console.log('env');
   return res.json({
     DB_HOST: process.env.DB_HOST,
     DB_PASSWORD: process.env.DB_PASSWORD,
     DB_USER: process.env.DB_USER,
     DB_DATABASE: process.env.DB_DATABASE,
   });
+})
+
+router.get('/', async (req, res, next) => {
+  console.log('/');
+  const promisePool = pool.promise();
+  const [ rows, fields ] = await promisePool.query('SELECT * FROM t');
+  return res.json(rows);
+});
+
+router.get('/save', async (req, res) => {
+  console.log('/save');
+  const { name } = req.query;
+
+  const promisePool = pool.promise();
+  const [ rows, fields ] = await promisePool.query('INSERT INTO t (name) VALUES(?)', [name]);
+
+  return res.json(rows);
 })
 
 module.exports = router;
